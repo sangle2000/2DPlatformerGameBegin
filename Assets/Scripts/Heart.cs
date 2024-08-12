75417be8ab9a7d9b331a6a8fd0f7a19a922901eb
sangle <thanhsang22440011@gmail.com>
2024-08-12 22:12:54 +0700

    Update Hurt and Dead logic and animation. Update Player Health logic and Health Bar UI

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Heart : MonoBehaviour
+{
+    [SerializeField] private float startingHeart;
+
+    public float CurrentHeart { get; private set; }
+
+    private Animator _anim;
+    private bool _dead;
+
+    private void Awake()
+    {
+        CurrentHeart = startingHeart;
+        _anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float damage)
+    {
+        CurrentHeart = Mathf.Clamp(CurrentHeart - damage, 0, startingHeart);
+        
+        if (CurrentHeart > 0)
+        {
+            _anim.SetTrigger("hurt");
+            // iframes
+        }
+        else
+        {
+            if (!_dead)
+            {
+                _anim.SetTrigger("die");
+                GetComponent<PlayerMovement>().enabled = false;
+                _dead = true;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
+    }
+}
